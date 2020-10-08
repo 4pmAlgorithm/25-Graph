50b3838f4454c1be28c9dddbe59627c22cef82bb
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-10-08 15:17:25 -0400

    initial commit

---
+++ graph.js
@@ -0,0 +1,21 @@
+//any social network
+//location/mapping
+//routing algorithm
+//visual hierarchy
+//file system optimization
+//netflix - recommendation "you might also like..."
+//"people you might know"
+//"frequently bought with"
+//https://musicmap.info/
+
+//NOT 2dimentional chart 
+
+//graph definition:
+//Nodes + Connections
+
+
+//vertex: a node
+//edge: connection between nodes
+//weighted/unweighted: values assigned to distances (edge/connection)
+//directied/undirected: directions assigned to distanced between vertices (ex: instagram:who follows who)
+
