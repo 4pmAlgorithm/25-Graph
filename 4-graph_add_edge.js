c7f8adfb3fb55e6dfef57e42b69e06c8d11e2bac
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-10-16 11:23:08 -0400

    .filter =>

--- 4-graph_add_edge.js
+++ 4-graph_add_edge.js
@@ -1,17 +1,38 @@
-class Graph{
+// class Graph{
+//     constructor(){
+//         this.adjacencyList = {};
+//     }
+//     addVertex(vertex){
+//         if(!this.adjacencyList[vertex]) {
+//            this.adjacencyList[vertex] = [];
+//          }
+//        }
+//     addEdge(v1,v2){
+//         this.adjacencyList[v1].push(v2);
+//         this.adjacencyList[v2].push(v1);
+//     }
+// }
+
+
+class Graph {
     constructor(){
-        this.adjacencyList = {};
+        this.adjacencyList = {}
     }
     addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+        if(!this.adjacencyList[vertex]){
+        this.adjacencyList[vertex] = []
+        }
     }
-    addEdge(v1,v2){
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)
     }
 }
+
 
 let g = new Graph();
 g.addVertex("Dallas");
 g.addVertex("Tokyo");
 g.addVertex("Aspen");
+g.addEdge("Dallas", "Aspen")
+console.log(g)
--- 5-graph_remove_edge.js
+++ 5-graph_remove_edge.js
@@ -9,12 +9,18 @@ class Graph{
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
-    removeEdge(vertex1,vertex2){
-        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-            v => v !== vertex2
+    removeEdge(vertex1, vertex2){ //Dallas, aspen
+        
+        console.log(vertex2)
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter( //filter only grab that under context
+            function (v) {
+                console.log("::::::v", v) //Dallas, Aspen 
+                console.log("!!", v !== vertex2) //Dallas True, Aspen false
+                return v !== vertex2 //grab only thet true, which is Dallas. Remove Aspen
+            }
         );
         this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-            v => v !== vertex1
+            v => v !== vertex1 //remove Dallas
         );
     }
 }
@@ -25,4 +31,6 @@ g.addVertex("Tokyo");
 g.addVertex("Aspen");
 g.addEdge("Dallas", "Tokyo");
 g.addEdge("Dallas", "Aspen");
-
+console.log(g)
+g.removeEdge("Dallas", "Aspen")
+console.log(g)
